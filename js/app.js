9a2dc258c05c0ce8d6cfd6377c64f6d512efcaba
DaylinYP <121044734+DaylinYP@users.noreply.github.com>
2025-05-07 07:36:57 -0600

    Total cambios

--- js/app.js
+++ js/app.js
@@ -199,7 +199,7 @@ function actualizarTotalCarrito(){
     }
     total = Math.round(total * 100)/100;
 
-    document.getElementsByClassName('carrito-precio-total')[0].innerText = '$'+total.toLocaleString("es") + ",00";
+    document.getElementsByClassName('carrito-precio-total')[0].innerText = 'Q'+total.toLocaleString("es") + ",00";
 
 }
 
